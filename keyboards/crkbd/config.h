cbecb2203884ee9cf7f838ef0213962e66953364
asdfire1 <45483619+asdfire1@users.noreply.github.com>
2022-10-11 22:27:17 +0200

    luna

--- keyboards/crkbd/config.h
+++ keyboards/crkbd/config.h
@@ -52,14 +52,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* disable debug print */
-// #define NO_DEBUG
+//#define NO_DEBUG
 
 /* disable print */
-// #define NO_PRINT
+//#define NO_PRINT
 
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+    #define NO_ACTION_ONESHOT
 
 #define USE_SERIAL
